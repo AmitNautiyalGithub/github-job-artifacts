c909c14452e44932265c2748450ace25284599bc
AmitNautiyal <59432251+AmitNautiyalGithub@users.noreply.github.com>
2026-03-12 14:26:40 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import App from "./App";
+import "./index.css";
+// this is important line
+// this is important line 2
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
